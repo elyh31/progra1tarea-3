f6864068463d62e0f07313ac2f6118a76b4542ee
elyh31 <elyh31@unitec.edu>
2014-09-22 08:09:16 -0600

    tarea 3

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -4,17 +4,20 @@ public class Ejercicios
 	//devuelve la suma de x y y
 	static int sumar(int x, int y)
 	{
+		//suma el valor x mas el valor y
 		return x + y;
 	}
 	
 	//devuelve la resta de x y y
 	static int restar(int x, int y)
 	{
+		//resta el valor x menos el valor y
 		return x - y;
 	}
 	//devuelve la multiplicacion de x y y
 	static int multiplicar(int x, int y)
 	{
+		//multiplica el valor x por el valor y
 		return x * y;
 	}
 	
@@ -23,6 +26,7 @@ public class Ejercicios
 	//https://mx.answers.yahoo.com/question/index?qid=20121107181832AAX979g
 	static int residuo(int x, int y)
 	{
+		//saca el residuo del valor x y el valor y
 		return x % y;
 	}
 	
@@ -31,6 +35,7 @@ public class Ejercicios
 	//http://latecladeescape.com/t/Algoritmo+para+saber+si+un+n%C3%BAmero+es+par+o+impar,+o+de+como+enrollarse+con+un+tema+de+lo+m%C3%A1s+tonto
 	static boolean esPar(int x)
 	{
+		//si el residuo es 0 dice verdadero sy es 1 dice falso
 		return  x % 2 == 0;
 	}
 	
@@ -38,18 +43,21 @@ public class Ejercicios
 	//todos los demas problemas los hice guiandome de lo que aprendi de la pagina anterios dada
 	static boolean esMultiploDe3(int x)
 	{
+		//si el residuo es 0 dice verdadero sy es 1 dice falso
 		return x % 3 == 0;
 	}
 	
 	//devuelve el numero mayor (x o y)
 	static int getMayor(int x,int y)
 	{
+		
 	 if (x > y) 
-	
+	//si el valor de x es mayor me devoelde x
 		return x;
 	
 	 
 		 else 
+		 //si el valor de y es mayor me devoelde y
 			 return y;
 		  		
 	}
@@ -57,12 +65,14 @@ public class Ejercicios
 	//devuelve true si la edad es mayor o igual a 18
 	static boolean esMayorDeEdad(int edad)
 	{
+		//si la edad es mayor de 18 dice verdadero sy es menor dice falso
 		return edad >= 18;
 	}
 	
 	//devuelve true si x, y y z son pares
 	static boolean sonPares(int x, int y, int z)
 	{
+		//si el residuo de los tres valores es 0 dice verdadero sy alguno es 1 dice falso
 		return x % 2 == 0 & y % 2 == 0 & z % 2 == 0;
 	}
 	
@@ -71,14 +81,17 @@ public class Ejercicios
 {	
 		if (x > y & x > z)
 		{
+			//si el valor de x es mayor me devoelde x
 			return x;
 		}
 		if (y > x & y > z)
 		{
+			//si el valor de y es mayor me devoelde y
 			return y;
 	    }
 		else
 		{
+			//si el valor de z es mayor me devoelde z
 			return z;
 		}
 }
